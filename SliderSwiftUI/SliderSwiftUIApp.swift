c384c65deb48187495ce7336f38ecb108152b62d
Eugenia <pyssiki@yandex.ru>
2022-05-23 19:54:02 +0500

    Initial Commit

---
+++ SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Евгения Аникина on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
